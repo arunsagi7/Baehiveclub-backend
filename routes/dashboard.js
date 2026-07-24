c0bcb4381ff201289d2488dbcc031e2e1bbfd086
Karthi Ram <karthi@billiontags.co>
2026-07-24 16:24:07 +0530

    remove nodemodules

--- routes/dashboard.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { verifyToken } = require('../middlewares/auth');
-const { getDashboardStats } = require('../controllers/dashboardController');
-
-const router = express.Router();
-
-router.get('/', verifyToken, getDashboardStats);
-
-module.exports = router;
